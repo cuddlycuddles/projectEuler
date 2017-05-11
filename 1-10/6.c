038652aad0785560d1167bd0c0444f4434ccb937
cuddlycuddles <stanley.su@y7mail.com>
2017-05-11 19:06:08 +1000

    problem 6

--- 1-10/6.c
+++ 1-10/6.c
@@ -1,30 +1,24 @@
+
 #include <stdio.h>
 
-long long sumSquares (int max);
-long long squareSum (int max);
+long long sumSquares (int n);
+long long squareSum (int n);
 
 int main (int argc, char *argv[]) {
-	int max = 100;
+	int n = 100;
 	
 	printf ("Difference between sumSquares and squareSum of %i = %lli\n",
-			max, sumSquares(max) - squareSum(max));
+			n, sumSquares(n) - squareSum(n));
 	
 	return 0;
 }
 
-// returns sum of squares of 1 to max
-long long sumSquares (int max) {
-	long long sum = 0;
-	int i = 1;
-	while (i <= max) {
-		sum += (i * i);
-		++i;
-	}
-	return sum;
+// returns sum of squares of 1 to n
+long long sumSquares (int n) {
+	return n * (n + 1) * (2 * n + 1) / 6;
 }
 
-// return square of sum of 1 to max
-long long squareSum (int max) {
-	long long sum = (1 + max) * (max / 2);
-	return sum * sum;
+// return square of sum of 1 to n
+long long squareSum (int n) {
+	return (n * n) * (n + 1) * (n + 1) / 4;
 }
