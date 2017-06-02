834ad3c7a50356bac2c2c05899813817ad17b2f5
Stanley Su <stanley.su@y7mail.com>
2017-06-02 22:51:49 +1000

    Update 12.c

--- 11-20/12.c
+++ 11-20/12.c
@@ -1,15 +1,29 @@
 #include <stdio.h>
+#include <assert.h>
 #include <math.h>
 
+void testFunctions (void);
 int numFactors (int n);
 
 int main (int argc, char *argv[]) {
+	testFunctions();
 	int i = 8;
 	int triNum = 28;
 	while (numFactors (triNum) < 500) { // get the number that has over 500 divisors
 		triNum += i++;
 	}
 	printf ("%i\n", triNum);
+}
+
+void testFunctions (void) {
+	assert (numFactors (142752) == 24);
+	assert (numFactors (183843) == 16);
+	assert (numFactors (20) == 6);
+	assert (numFactors (320) == 14);
+	assert (numFactors (1846206) == 32);
+	assert (numFactors (142857) == 32);
+	assert (numFactors (149459) == 2);
+	printf ("Functions work.\n");
 }
 
 // returns the number of factors a number has (doesn't work on smaller numbers e.g. 1 to 4)
