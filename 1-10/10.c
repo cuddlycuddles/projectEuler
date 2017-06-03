ea31c56381393432557e663a54ede72c7ab03e45
cuddlycuddles <stanley.su@y7mail.com>
2017-06-03 14:29:59 +1000

    refactored problem 10

--- 1-10/10.c
+++ 1-10/10.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
+#include <assert.h>
 #include <math.h>
 
 #define LIMIT 2000000
 
+void testFunctions (void);
 int isPrime (int x);
 
 int main (int argc, char *argv[]) {
+	testFunctions();
 	unsigned long long sum = 2;
 	int curr = 3;
 	while (curr < LIMIT) {
@@ -18,6 +21,18 @@ int main (int argc, char *argv[]) {
 	return 0;
 }
 
+void testFunctions (void) {
+	assert (isPrime (142857) == 0);
+	assert (isPrime (20)	 == 0);
+	assert (isPrime (25)	 == 0);
+	assert (isPrime (9801)	 == 0);
+	assert (isPrime (1009)	 == 1);
+	assert (isPrime (1567)	 == 1);
+	assert (isPrime (7919)	 == 1);
+	assert (isPrime (104729) == 1);
+	printf ("Functions work.\n");
+}
+
 int isPrime (int x) {
 	int i = 2;
 	int root = (int) sqrt (x);
@@ -25,7 +40,7 @@ int isPrime (int x) {
 		return 0;
 	}
 	++i;
-	while (i <= root) {
+	while (i <= root) { // only need to check until square root of input
 		if (x % i == 0) {
 			return 0;
 		}
