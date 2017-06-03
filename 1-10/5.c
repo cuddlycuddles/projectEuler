20567e4126d9f07e51a4f59e972a5fa46dc9fb69
cuddlycuddles <stanley.su@y7mail.com>
2017-06-03 14:23:51 +1000

    refactored problem 5

--- 1-10/5.c
+++ 1-10/5.c
@@ -1,21 +1,30 @@
 #include <stdio.h>
+#include <assert.h>
 
+void testFunctions (void);
 long gcd (long x, int y);
 long lcm (long x, int y);
 
 int main (int argc, char *argv[]) {
+	testFunctions();
 	int i = 2;
 	long result = 1;
-	
 	while (i <= 20) {
-		result = lcm (result, i);
-		
-		++i;
+		result = lcm (result, i++);
 	}
-	
 	printf ("Smallest common multiple = %li\n", result);
-	
 	return 0;
+}
+
+// test lcm and gcd
+void testFunctions (void) {
+	assert (lcm (1, 2) == 2);
+	assert (lcm (999999, 142857) == 999999);
+	assert (lcm (1513, 1531) == 2316403);
+	assert (gcd (120, 24) == 24);
+	assert (gcd (250, 25) == 25);
+	assert (gcd (9801, 9603) == 99);
+	printf ("Functions work.\n");
 }
 
 // calculates greatest common divisor of two numbers
@@ -33,5 +42,5 @@ long gcd (long x, int y) {
 // by dividing their product
 // by their greatest common divisor
 long lcm (long x, int y) {
-	return x / gcd(x, y) * y;
+	return (x * y) / gcd(x, y);
 }
