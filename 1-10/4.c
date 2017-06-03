0c91a013a934250fb10912adb47f0e928da71fe9
cuddlycuddles <stanley.su@y7mail.com>
2017-06-03 14:47:40 +1000

    refactored problem 4

--- 1-10/4.c
+++ 1-10/4.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
+#include <assert.h>
 
+void testFunctions (void);
 long long palindrome (long long x);
 
 int main (int argc, char *argv[]) {
+	testFunctions();
 	int x = 101;
 	int y;
 	long long largest = 0;
@@ -19,6 +22,18 @@ int main (int argc, char *argv[]) {
 	printf ("Largest palindrome = %lli\n", largest);
 	
 	return 0;
+}
+
+void testFunctions (void) {
+	assert (palindrome (1337)			== 0);
+	assert (palindrome (10)				== 0);
+	assert (palindrome (142857)			== 0);
+	assert (palindrome (10881)			== 0);
+	assert (palindrome (9009)			== 9009);
+	assert (palindrome (2552)			== 2552);
+	assert (palindrome (1324231)		== 1324231);
+	assert (palindrome (1234567654321)	== 1234567654321);
+	printf ("Functions work.\n");
 }
 
 // returns original number if it is palindrome, else returns 0
